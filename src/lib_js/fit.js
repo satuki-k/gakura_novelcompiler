fd4edc6a390c46c66617d7361b8d1fdfc19d0509
satuki-k <stkli@outlook.jp>
2025-10-04 21:25:21 +0900

    メディアクエリという苦肉の策が不要になったので文字サイズ係数を増加

--- src/lib_js/fit.js
+++ src/lib_js/fit.js
@@ -2,7 +2,7 @@
 
 //ウィンドウサイズに応じてフォントサイズを変更
 function fit(){
-	$ID("gaku-ura_base").style.fontSize = Math.max(12, Math.min(window.innerHeight*0.02, window.innerWidth*0.02))+"px";
+	$ID("gaku-ura_base").style.fontSize = Math.max(12, Math.min(window.innerHeight*0.03, window.innerWidth*0.03))+"px";
 }
 fit();
 window.addEventListener("resize", fit);
